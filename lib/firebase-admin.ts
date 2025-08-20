1d403e9700a563741a6312a5fd353cffd668e537
neesh0120 <dharaneesh077@gmail.com>
2025-08-21 01:51:00 +0530

    Fix: Correctly format Firebase private key

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,19 +1,24 @@
 /// <reference types="firebase-admin" />
 import admin from 'firebase-admin';
 
+let app;
+
 if (!admin.apps.length) {
   try {
-    admin.initializeApp({
+    app = admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       }),
     });
   } catch (error) {
-    console.error('Firebase admin initialization error', error);
+    console.error('Firebase admin initialization error:', error);
+    throw error; // Re-throw the error to fail the build explicitly
   }
+} else {
+  app = admin.app();
 }
 
-export const authAdmin = admin.auth();
-export const dbAdmin = admin.firestore();
+export const authAdmin = admin.auth(app);
+export const dbAdmin = admin.firestore(app);
